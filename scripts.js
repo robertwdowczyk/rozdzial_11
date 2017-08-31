b4a65b5391d1f5e59a49b1abce1df1f8f915fbcd
robertwdowczyk <30527317+robertwdowczyk@users.noreply.github.com>
2017-08-31 19:22:59 +0200

    Trello

--- scripts.js
+++ scripts.js
@@ -1,18 +1,128 @@
-﻿function Button(text) {
-    this.text = text || 'Hello';
-}
-Button.prototype = {
-    create: function() {
-    var self = this;
-    this.$element = $('<button>');
-    this.$element.text(this.text);
-    this.$element.click(function() {
-      alert(self.text);
-      
-      });
-    $('body').append(this.$element);
-  }
-}
+﻿$(document).ready(function(){
+    function randomString() { 
+        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'; 
+        var str = ''; 
+        for (i = 0; i < 10; i++) { 
+                str += chars[Math.floor(Math.random() * chars.length)]; 
+            } 
+            return str; 
+        }
+    function Column(name) { 
+        var self = this; 
+        
+        this.id = randomString(); 
+        this.name = name; 
+        this.$element = createColumn(); 
+    
+        function createColumn() { 
+            
+            var $column = $('<div>').addClass('column'); 
+            var $columnTitle = $('<h2>').addClass('column-title').text(self.name); 
+            var $columnCardList = $('<ul>').addClass('column-card-list'); 
+            var $columnDelete = $('<button>').addClass('btn-delete').text('x'); 
+            var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj kartę');
 
-var btn1 = new Button('Hello!');
-btn1.create();
+            $columnDelete.click(function() { 
+                self.removeColumn(); 
+            });
+            $columnAddCard.click(function() { 
+                self.addCard(new Card(prompt("Wpisz nazwę karty"))); 
+            });
+        
+            
+            $column.append($columnTitle) 
+                .append($columnDelete) 
+                .append($columnAddCard) 
+                .append($columnCardList);
+            
+            return $column;
+        }
+    }
+
+    Column.prototype = { 
+        addCard: function(card) { 
+            this.$element.children('ul').append(card.$element); 
+        }, 
+        removeColumn: function() { 
+            this.$element.remove(); 
+        } 
+    }; 
+
+    function Card(description) { 
+        var self = this; 
+
+        this.id = randomString(); 
+        this.description = description; 
+        this.$element = createCard(); 
+
+        function createCard() { 
+            var $card = $('<li>').addClass('card'); 
+            var $cardDescription = 
+                $('<p>').addClass('card-description').text(self.description); 
+            var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+        
+            $cardDelete.click(function(){ 
+                self.removeCard(); 
+            });
+
+            $card.append($cardDelete) 
+                    .append($cardDescription); 
+            return $card;
+        } 
+    }
+
+    Card.prototype = { 
+        removeCard: function() { 
+            this.$element.remove(); 
+        } 
+    };
+
+    var board = { 
+        name: 'Tablica Kanban', 
+        addColumn: function(column) { 
+            this.$element.append(column.$element); 
+            initSortable();
+        }, 
+        $element: $('#board .column-container') 
+    };
+
+    function initSortable() { 
+        $('.column-card-list').sortable({ 
+            connectWith: '.column-card-list', 
+            placeholder: 'card-placeholder' 
+        }).disableSelection(); 
+    }
+
+    $('.create-column') 
+        .click(function(){ 
+            var name = prompt('Wpisz nazwę kolumny'); 
+            var column = new Column(name); 
+            board.addColumn(column); 
+        });
+
+    
+    var todoColumn = new Column('Do zrobienia'); 
+    var doingColumn = new Column('W trakcie'); 
+    var doneColumn = new Column('Skończone');
+    var toCheckColumn = new Column('Do sprawdzenia'); 
+
+
+    board.addColumn(todoColumn); 
+    board.addColumn(doingColumn); 
+    board.addColumn(doneColumn);
+    board.addColumn(toCheckColumn); 
+
+
+    var card1 = new Card('Zakupy :)'); 
+    var card2 = new Card('Zatankowac samochod');
+    var card3 = new Card('Nuaka japonskiego');
+    var card4 = new Card('Tenis obejrzec');
+    var card5 = new Card('Umyc się') 
+
+
+    todoColumn.addCard(card1); 
+    doingColumn.addCard(card2);
+    todoColumn.addCard(card3);
+    toCheckColumn.addCard(card5);
+    doneColumn.addCard(card4);
+});
